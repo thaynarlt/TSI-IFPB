25a1e828455651b69de6a9fc9f498055cee22437
Thayna Tolentino <tatarlt7@gmail.com>
2023-12-19 14:33:46 -0300

    sql

--- P2/Banco de Dados I/estudando.sql
+++ P2/Banco de Dados I/estudando.sql
@@ -191,4 +191,110 @@ INNER JOIN Funcao F2 ON F1.idfuncao = F2.idfuncao
 WHERE F2.nome LIKE 'Diretor%';
 
 #22. Obter o código do pedido, o nome do cliente e o nome do funcionário de todos os pedidos.
-SELECT 
+SELECT P.idpedido AS 'Pedido',
+	   C.nome AS 'Cliente',
+       F.nome AS 'Funcionário'
+FROM Pedido P
+INNER JOIN Cliente C ON P.idcliente = C.idcliente
+INNER JOIN Funcionario F ON P.idvendendor = F.idfuncionario;
+
+#23. Obter a quantidade de funcionários agrupados pelo nome do setor, mostrando apenas os setores com mais de 5 funcionários.
+SELECT S.nome AS 'Setor', 
+       COUNT(*) AS 'Quantidade'
+FROM Funcionario F
+INNER JOIN Setor S ON F.idsetor = S.idsetor
+GROUP BY S.nome
+HAVING COUNT(*) > 5;
+
+#24. Obter o código e o nome das cidades que não tenham funcionários residentes nelas, ordenado pelo nome da cidade em ordem ascendente.
+SELECT C.idcidade AS 'Código da Cidade', 
+       C.nome As 'Nome da Cidade'
+FROM Cidade C
+LEFT JOIN Funcionario F ON C.idcidade = F.idreside
+WHERE F.idfuncionario IS NULL
+ORDER BY C.nome; 
+
+#25. Obter o nome do setor superior e a quantidade de setores subordinados a cada um deles, ordenados pela quantidade em ordem descendente.
+SELECT S2.nome AS 'Setor Superior',
+       COUNT(*) AS 'Quantidade de Setores Subordinados'
+FROM Setor S1
+INNER JOIN Setor S2
+ON S1.idsuperior = S2.idsetor
+GROUP BY S2.nome
+ORDER BY COUNT(*) DESC;
+
+#SCRIPT INICIAL (DDL)
+CREATE DATABASE BD_EX_DML;
+USE BD_EX_DML;
+CREATE TABLE Emp
+(
+codigo INT NOT NULL,
+nome VARCHAR(30) NOT NULL PRIMARY KEY,
+sexo CHAR(1) NOT NULL,
+fone VARCHAR(16) NULL,
+cidade VARCHAR(20) NULL DEFAULT 'João Pessoa',
+salario NUMERIC(10,2) NULL
+);
+CREATE TABLE Emp2
+(
+codigo INT NOT NULL PRIMARY KEY,
+nome VARCHAR(30) NOT NULL,
+salario NUMERIC(10,2)
+);
+
+
+#1. Inserir, na tabela Emp, os dados:
+INSERT INTO Emp (codigo, nome,sexo,fone,cidade,salario)
+VALUES (620,'Antonio Pereira','M','3212-1212',DEFAULT,5000.00),
+       (371,'Flávio Santana','M','3245-4545','Recife',NULL),
+       (209,'Maria de Fátima','F','3232-3232','Natal',3000.00),
+       (110,'Carlos Alberto','M',NULL,'Recife',NULL),
+       (555,'Maria Aparecida','F',NULL,DEFAULT,4000.00);
+
+#2. Alterar, na tabela Emp, o telefone do empregado de código 555 para “3222-9999”.
+UPDATE Emp
+SET fone = '3222-9999'
+WHERE codigo = 555;
+
+#3. Colocar, na tabela Emp, o salário de R$ 1.500,00 para os empregados que estejam sem salário.
+UPDATE Emp
+SET salario = 1500.00
+WHERE salario IS NULL;
+
+#4. Alterar, na tabela Emp, o telefone do empregado de código 110 para “99988-7766” e o salário para R$ 2.000,00.
+UPDATE Emp
+SET fone = '99988-7766', salario = 2000.00
+WHERE codigo = 110;
+
+#5. Adicionar R$ 150,00 ao salário dos empregados da tabela Emp que não moram em "João Pessoa".
+UPDATE Emp
+SET salario = salario + 150.00
+WHERE cidade != 'João Pessoa';
+
+#6. Alterar o salário dos empregados da tabela Emp para que nenhum deles ultrapasse R$ 3.500,00. Ou seja, os que ganharem acima disso vão passar a ter o salário com esse valor.
+UPDATE Emp
+SET salario = 3500.00
+WHERE salario > 3500.00;
+
+#7. Inserir, na tabela Emp, um novo empregado com código 400, nome 'Francisco de Assis', sexo masculino e salário igual ao menor salário cadastrado.
+INSERT INTO Emp (codigo, nome,sexo,salario)
+SELECT 400, 'Francisco de Assis','M', MIN(salario) FROM Emp;
+
+#8. Preencher a tabela Emp2 com os empregados do sexo feminino cadastrados na tabela Emp.
+INSERT INTO Emp2 (codigo, nome, salario)
+SELECT codigo,nome,salario FROM Emp
+WHERE sexo = 'F';
+
+#9. Excluir, na tabela Emp, todos os empregados do sexo feminino.
+DELETE FROM Emp
+WHERE sexo = 'F';
+
+#10. Excluir todos os empregados da tabela Emp2.
+DELETE FROM Emp2;
+
+
+
+
+
+
+
